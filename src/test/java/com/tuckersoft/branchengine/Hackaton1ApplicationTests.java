535c2545088401d2f4f14634f92e167e8d326b68
Valentinapuicon <valentina.puicon@utec.edu.pe>
2026-09-21 18:32:50 -0500

    Proyecto base Spring Boot 3.3.4

---
+++ src/test/java/com/tuckersoft/branchengine/Hackaton1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuckersoft.branchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
